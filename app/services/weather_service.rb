0a0afc9c8e1fb8b28f8a5a14be7f3722d2c5771b
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-06 14:35:04 -0600

    Create WeatherService to handle the requests to OpenWeatherAPI

---
+++ app/services/weather_service.rb
@@ -0,0 +1,18 @@
+class WeatherService
+  def get_forecast_info
+    params = { lat: '39.7392', lon: '-104.9903', unit: 'imperial'}
+    get_json('data/2.5/onecall', params)
+  end
+
+  private
+
+  def get_json(url, params)
+    conn.get(url, params)
+  end
+
+  def conn
+    Faraday.new(url: 'https://api.openweathermap.org') do |f|
+      f.params['appid'] = 'f3fa19a39c077c30289cf9672ad2f599'
+    end
+  end
+end
