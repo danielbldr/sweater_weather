b6d873b0591609485f58f63c68df0aa957ce02e5
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-06 16:47:51 -0600

    Refactor controller index action with weather results

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    weather_service = WeatherService.new
-    response = weather_service.get_forecast_info
+    # weather_service = WeatherService.new
+    # response = weather_service.get_forecast_info
+    weather_results = WeatherResults.new
+    response = weather_results.get_local_weather
+
     render json: response.body
   end
 end
