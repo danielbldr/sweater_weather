e5c500ed8a79337b3496ed632e7e2ad57c1fcc47
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-08 10:53:58 -0600

    Create Restaurant class with name and address attributes

---
+++ app/poros/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant
+  attr_reader :name, :address
+
+  def initialize(data)
+    @name = data[:restaurants].first[:restaurant][:name]
+    @address = data[:restaurants].first[:restaurant][:location][:address]
+  end
+end
