58eafc688bc35806c81490d7da8b2b9b83f22f2f
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-08 09:44:57 -0600

    Create get_forecast_summary to return a ForecastSummary object

--- app/poros/forecast_results.rb
+++ app/poros/forecast_results.rb
@@ -4,4 +4,10 @@ class ForecastResults
     response = forecast_service.get_forecast_info(lat, lon)
     CityForecast.new(response)
   end
+
+  def get_forecast_summary(lat, lon)
+    forecast_service = ForecastService.new
+    response = forecast_service.get_forecast_info(lat, lon)
+    ForecastSummary.new(response)
+  end
 end
