19967ad249c76ecb9a2bf2f0f8d11de8ac928939
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-08 13:42:54 -0600

    Encapsulated the geocode service in the forecast results

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,11 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    geocode_service = GeocodeService.new
-    coords = geocode_service.get_coordinates(params[:location])
-
     forecast_results = ForecastResults.new
-    city_forecast = forecast_results.get_local_weather(coords[:lat],
-                                                       coords[:lng])
+    city_forecast = forecast_results.get_local_weather(params[:location])
 
     render json: ForecastSerializer.new(city_forecast).serializable_hash
   end
--- app/poros/forecast_results.rb
+++ app/poros/forecast_results.rb
@@ -1,7 +1,10 @@
 class ForecastResults
-  def get_local_weather(lat, lon)
+  def get_local_weather(location)
+    geocode_service = GeocodeService.new
+    coords = geocode_service.get_coordinates(location)
+
     forecast_service = ForecastService.new
-    response = forecast_service.get_forecast_info(lat, lon)
+    response = forecast_service.get_forecast_info(coords[:lat], coords[:lng])
     CityForecast.new(response)
   end
 end
