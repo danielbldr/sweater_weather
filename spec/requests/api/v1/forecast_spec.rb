797eb5a0aedfd9a3350ba29ef6d8efa1dd744cff
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-07 19:47:06 -0600

    Update spec to find keys in expected response

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -6,12 +6,34 @@ RSpec.describe "Forecast Endpoints" do
 
     expect(response).to be_successful
     weather_info = JSON.parse(response.body, symbolize_names: true)
-    expect(weather_info[:lat]).to eq(39.74)
-    expect(weather_info[:lon]).to eq(-104.99)
-    expect(weather_info[:timezone]).to eq('America/Denver')
+    weather_attributes = weather_info[:data][:attributes]
 
-    expect(weather_info).to have_key(:current)
-    expect(weather_info).to have_key(:hourly)
-    expect(weather_info).to have_key(:daily)
+    expect(weather_attributes).to have_key(:hourly)
+    expect(weather_attributes).to have_key(:seven_day)
+    expect(weather_attributes).to have_key(:daily)
+
+    expect(weather_attributes[:hourly].count).to eq(8)
+    expect(weather_attributes[:seven_day].count).to eq(7)
+
+    expect(weather_attributes[:hourly].first).to have_key(:time)
+    expect(weather_attributes[:hourly].first).to have_key(:current_temp)
+    expect(weather_attributes[:hourly].first).to have_key(:description)
+
+    expect(weather_attributes[:seven_day].first).to have_key(:description)
+    expect(weather_attributes[:seven_day].first).to have_key(:precipitation)
+    expect(weather_attributes[:seven_day].first).to have_key(:high)
+    expect(weather_attributes[:seven_day].first).to have_key(:low)
+    expect(weather_attributes[:seven_day].first).to have_key(:day)
+
+    expect(weather_attributes[:daily]).to have_key(:current_temp)
+    expect(weather_attributes[:daily]).to have_key(:high)
+    expect(weather_attributes[:daily]).to have_key(:low)
+    expect(weather_attributes[:daily]).to have_key(:description)
+    expect(weather_attributes[:daily]).to have_key(:feels_like)
+    expect(weather_attributes[:daily]).to have_key(:humidity)
+    expect(weather_attributes[:daily]).to have_key(:visibility)
+    expect(weather_attributes[:daily]).to have_key(:uv_index)
+    expect(weather_attributes[:daily]).to have_key(:sunrise)
+    expect(weather_attributes[:daily]).to have_key(:sunset)
   end
 end
