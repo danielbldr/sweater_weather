a016de45d276bf5121d07642144a77cb02cf0a76
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-08 10:34:38 -0600

    Abstracted the API call to get direction info out into Direction Service

---
+++ app/services/direction_service.rb
@@ -0,0 +1,21 @@
+class DirectionService
+  def get_travel_time(origin, destination)
+    params = { origin: origin, destination: destination}
+    directions = get_json('/maps/api/directions/json', params)
+
+    directions[:routes].first[:legs].first[:duration][:text]
+  end
+
+  private
+  
+  def get_json(url, params)
+    response = conn.get(url, params)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://maps.googleapis.com') do |f|
+      f.params[:key] = ENV['GOOGLE_API']
+    end
+  end
+end
