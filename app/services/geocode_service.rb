9ee33391c759829858cc0439e89afd8133b3968a
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-07 22:02:43 -0600

    Create geocode service to handle api calls to geocode api

---
+++ app/services/geocode_service.rb
@@ -0,0 +1,20 @@
+class GeocodeService
+  def get_coordinates(location)
+    params = { address: location }
+    response = get_json('/maps/api/geocode/json', params)
+    response[:results].first[:geometry][:location]
+  end
+
+  private
+
+  def get_json(url, params)
+    response = conn.get(url, params)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://maps.googleapis.com') do |f|
+      f.params[:key] = ENV['GEOCODE_API']
+    end
+  end
+end
