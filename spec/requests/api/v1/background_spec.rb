93ec8d63707a53e5893338909aab8366ea1b556c
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-08 15:34:17 -0600

    Test background endpoint can return a photo of city

---
+++ spec/requests/api/v1/background_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Background Endpoints" do
+  it "can retreive url for background image" do
+    get '/api/v1/backgrounds?location=denver,co'
+
+    expect(response).to be_successful
+    url = JSON.parse(response.body, symbolize_names: true)
+
+    expect(url[:data][:attributes]).to have_key(:image)
+  end
+end
