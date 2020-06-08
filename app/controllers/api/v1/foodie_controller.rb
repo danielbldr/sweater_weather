9705ec85cfd2982d2b76a115e5184cf6d80d6121
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-08 11:06:39 -0600

    Refactor controller to push getting info from services logic to the Foodie poro

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -1,36 +1,12 @@
 class Api::V1::FoodieController < ApplicationController
   def index
-    direction_service = DirectionService.new
-    time = direction_service.get_travel_time(params[:start], params[:end])
-
-    geocode_service = GeocodeService.new
-    coords = geocode_service.get_coordinates(params[:end])
-
-    forecast_results = ForecastResults.new
-    forecast_summary = forecast_results.get_forecast_summary(coords[:lat],
-                                                             coords[:lng])
-
-    # conn = Faraday.new(url: 'https://developers.zomato.com') do |f|
-    #   f.headers['user-key'] = ENV['ZOMATO_API']
-    # end
-    #
-    # response = conn.get('/api/v2.1/search') do |req|
-    #   req.params[:lat] = coords[:lat]
-    #   req.params[:lon] = coords[:lng]
-    #   req.params[:q] = params[:search]
-    # end
-    #
-    # restaurants = JSON.parse(response.body, symbolize_names: true)
-    #
-    # name = restaurants[:restaurants].first[:restaurant][:name]
-    # address = restaurants[:restaurants].first[:restaurant][:location][:address]
-    zomato_service = ZomatoService.new
-    restaurant = zomato_service.get_restaurant(coords[:lat], coords[:lng], params[:search])
-    params = {end_location: self.params[:end],
-              travel_time: time,
-              forecast: forecast_summary,
-              restaurant: { name: restaurant.name, address: restaurant.address }}
-    foodie = Foodie.new(params)
+    foodie = Foodie.new(foodie_params)
     render json: FoodieSerializer.new(foodie).serializable_hash
+  end
+
+  private
+
+  def foodie_params
+    params.permit(:start, :end, :search)
   end
 end
--- app/poros/foodie.rb
+++ app/poros/foodie.rb
@@ -2,10 +2,34 @@ class Foodie
   attr_reader :end_location, :travel_time, :forecast, :restaurant, :id
 
   def initialize(params)
-    @end_location = params[:end_location]
-    @travel_time = params[:travel_time]
-    @forecast = params[:forecast]
-    @restaurant = params[:restaurant]
+    @end_location = params[:end]
+    @travel_time = get_travel_time(params)
+    @forecast = get_forecast(params)
+    @restaurant = get_restaurant(params)
     @id = nil
+  end
+
+  private
+
+  def get_travel_time(params)
+    direction_service = DirectionService.new
+    direction_service.get_travel_time(params[:start], params[:end])
+  end
+
+  def get_coords(params)
+    geocode_service = GeocodeService.new
+    geocode_service.get_coordinates(params[:end])
+  end
+
+  def get_forecast(params)
+    coords = get_coords(params)
+    forecast_results = ForecastResults.new
+    forecast_results.get_forecast_summary(coords[:lat], coords[:lng])
+  end
+
+  def get_restaurant(params)
+    coords = get_coords(params)
+    zomato_service = ZomatoService.new
+    zomato_service.get_restaurant(coords[:lat], coords[:lng], params[:search])
   end
 end
