4c68058502a472985bb07c1a00ee9bb1a096e8b7
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-08 14:35:21 -0600

    Test it can get coordinates for a specific location

---
+++ spec/services/geocode_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe GeocodeService do
+  it 'can get coordinates of a city' do
+    goecode_service = GeocodeService.new
+    coords = goecode_service.get_coordinates('denver,co')
+
+    expect(coords[:lat]).to eq(39.7392358)
+    expect(coords[:lng]).to eq(-104.990251)
+  end
+
+  it 'can get coordinates of a different city' do
+    goecode_service = GeocodeService.new
+    coords = goecode_service.get_coordinates('irvine,co')
+
+    expect(coords[:lat]).to eq(33.6168308)
+    expect(coords[:lng]).to eq(-117.8715267)
+  end
+end
