a7a1ec321e7cecac24dde1364b386371a4a7049f
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-08 09:31:27 -0600

    Add foodie route

--- spec/requests/api/v1/foodie_spec.rb
+++ spec/requests/api/v1/foodie_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Foodie Endpoint' do
-  it 'returns end city' do
+  it 'returns end city, estimated time of travel, restaurant info, and forecast' do
     get '/api/v1/foodie?start=denver,co&end=pueblo,co&search=italian'
 
     expect(response).to be_successful
 
     foodie_info = JSON.parse(response.body, symbolize_names: true)
     foodie_data = foodie_info[:data]
-    foodie_attributes = foodie_data[:attributes]
-    foodie_forecast = foodie_attributes[:forecast]
-    foodie_restaurant = foodie_attributes[:restaurant]
+    attributes = foodie_data[:attributes]
+    forecast = attributes[:forecast]
+    restaurant = attributes[:restaurant]
 
-    expect(foodie_attributes).to have_key(:end_location)
-    expect(foodie_attributes).to have_key(:travel_time)
-    expect(foodie_attributes).to have_key(:forecast)
+    expect(attributes).to have_key(:end_location)
+    expect(attributes).to have_key(:travel_time)
+    expect(attributes).to have_key(:forecast)
 
-    expect(foodie_forecast).to have_key(:summary)
-    expect(foodie_forecast).to have_key(:temperature)
+    expect(forecast).to have_key(:summary)
+    expect(forecast).to have_key(:temperature)
 
-    expect(foodie_restaurant).to have_key(:name)
-    expect(foodie_restaurant).to have_key(:address)
+    expect(restaurant).to have_key(:name)
+    expect(restaurant).to have_key(:address)
   end
 end
