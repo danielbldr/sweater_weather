9e8304f06f45a1a1820dba6a2b89c56d09d3ba0a
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-09 10:02:38 -0600

    Create sessions controller with create action and success/error helper methods

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    return error('email') unless user
+
+    authenticated_user = user.authenticate(params[:password])
+    authenticated_user ? success(user) : error('password')
+  end
+
+  private
+
+  def success(user)
+    render json: UsersSerializer.new(user).serializable_hash
+  end
+
+  def error(error = 'password')
+    if error == 'password'
+      render json: { password: ['Invalid password'] }
+    else
+      render json: { email: ['Invalid email address'] }
+    end
+  end
+end
