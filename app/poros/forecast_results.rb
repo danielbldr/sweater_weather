e356a74f4d5eb9c10c50349f526da8e691a97673
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-09 12:00:34 -0600

    Create get_forecast_summary method to return summary of current forecast at destination

--- app/poros/forecast_results.rb
+++ app/poros/forecast_results.rb
@@ -7,4 +7,10 @@ class ForecastResults
     response = forecast_service.get_forecast_info(coords[:lat], coords[:lng])
     CityForecast.new(response)
   end
+
+  def get_forecast_summary(lat, lon)
+    forecast_service = ForecastService.new
+    response = forecast_service.get_forecast_info(lat, lon)
+    ForecastSummary.new(response)
+  end
 end
