b2930d96a5fbf3274f1312057c70b77690a0745a
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-06-14 09:16:40 -0700

    Update tests to expect values exists instead of an actual value that will change over time

--- spec/services/direction_service_spec.rb
+++ spec/services/direction_service_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe DirectionService do
     direction_service = DirectionService.new
     time = direction_service.get_travel_time('denver,co', 'pueblo,co')
 
-    expect(time.travel_time).to eq("1 hour 48 mins")
-    expect(time.seconds).to eq(6479)
+    expect(time.travel_time.class).to eq(String)
+    expect(time.seconds.class).to eq(Integer)
   end
 
   it 'can get coordinates of a different city', :vcr do
     direction_service = DirectionService.new
     time = direction_service.get_travel_time('denver,co', 'irvine,co')
 
-    expect(time.travel_time).to eq("15 hours 18 mins")
-    expect(time.seconds).to eq(54530)
+    expect(time.travel_time.class).to eq(String)
+    expect(time.seconds.class).to eq(Integer)
   end
 end
